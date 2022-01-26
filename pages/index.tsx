9a7b1c2ef099d48c4c1bb7d74c7bc036b76aac8e
alad2121 <alero.adegoroye@gmail.com>
2022-01-26 12:49:48 -0500

    Base code
    Starting code for the Poilitik website. This includes TailWindCSS, React, NextJs, and TypeScript.

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+
+export default function Home() {
+  return (
+    <div className="root">
+      <Head>
+        <title>Poilitik</title>
+        <link rel="icon" href="../static/favicon.ico" type="image/x-icon"/>
+      </Head>
+      <main>
+
+      </main>
+    </div>
+  );
+}
